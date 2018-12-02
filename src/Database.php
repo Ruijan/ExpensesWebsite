7750dee8a90ee58cb85fcb04f126e78b72c9a094
JulienR <rechenmann@gmail.com>
2018-12-02 22:48:13 +0100

    prevent security breach

--- src/Database.php
+++ src/Database.php
@@ -10,6 +10,7 @@ class Database
         $this->driver = $driver;
         $this->databaseName = $databaseName;
         $query = "CREATE DATABASE IF NOT EXISTS ".$databaseName.";";
+        $query = $this->driver->real_escape_string($query);
         if ($this->driver->query($query) === FALSE) {
             throw new \Exception("Couldn't create database ".$databaseName.".");
         }
@@ -30,6 +31,7 @@ class Database
 
     public function dropDatabase(){
         $query = "DROP DATABASE ".$this->databaseName.";";
+        $query = $this->driver->real_escape_string($query);
         if ($this->driver->query($query) === FALSE) {
             throw new \Exception("Couldn't drop database ".$this->databaseName.".");
         }
