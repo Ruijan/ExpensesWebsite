e32b85770a30dff545b41411cc4dc9334fb332a3
elyxa <elisabetta.messina@epfl.ch>
2019-01-12 21:23:35 +0100

    move code to a class

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -1,33 +1,42 @@
 <?php
 
-function createHead(){
-  return "<head>
-      <title>SignIn</title>
-      <link rel=\"stylesheet\" href=\"style.css\"
-    </head>";
+class IndexPage{
+  private $content;
+
+  public function display(){
+    echo $this->content;
+  }
+
+  public function __construct(){
+    $this->content = "<html>".$this->createHead().$this->createBody()."</html>";
+  }
+
+  private function createHead(){
+    return "<head>
+        <title>SignIn</title>
+        <link rel=\"stylesheet\" href=\"style.css\"
+      </head>";
+  }
+
+  private function createBody(){
+    return "<body>".$this->createHeader().$this->createButtons()."</body>";
+  }
+
+  private function createHeader(){
+    return "<div>
+      <h1 class=\"titleFont\">Expenses website</h1>
+      <h3>A penny saved is a penny earned.</br>
+       - <em>Benjamin Franklin</em></h3>
+    </div>";
+  }
+
+  private function createButtons(){
+    return "<div class=\"centeredButtons\">
+          <button class=\"btn SignIn\" onclick=\"location.href='signIn.php'\" type=\"button\">Sign In</button>
+          <button class=\"btn SignUp\" onclick=\"location.href='signUp.php'\" type=\"button\">Sign Up</button>
+        </div>";
+  }
 }
 
-function createHeader(){
-  return "<div>
-    <h1 class=\"titleFont\">Expenses website</h1>
-    <h3>A penny saved is a penny earned.</br>
-     - <em>Benjamin Franklin</em></h3>
-  </div>";
-}
-
-function createButtons(){
-  return "<div class=\"centeredButtons\">
-        <button class=\"btn SignIn\" onclick=\"location.href='signIn.php'\" type=\"button\">Sign In</button>
-        <button class=\"btn SignUp\" onclick=\"location.href='signUp.php'\" type=\"button\">Sign Up</button>
-      </div>";
-}
-
-function createBody(){
-  return "<body>".createHeader().createButtons()."</body>";
-}
-
-function createHtmlPage(){
-  return "<html>".createHead().createBody()."</html>";
-}
-
-echo createHtmlPage();
+$page = new IndexPage;
+$page->display();
