ccd046c0d745d59d66ebf16d930cd7816fd5b360
JulienR <rechenmann@gmail.com>
2019-06-04 21:20:46 +0200

    add display errors

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 session_start();
 require_once '../vendor/autoload.php';
 use BackEnd\Routing\Request\Connection\ConnectionRequestFactory;
